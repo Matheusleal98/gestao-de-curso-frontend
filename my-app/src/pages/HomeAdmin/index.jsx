27638597589fb3457929844379cdb2555bba18a0
leal <matheusskeal98@gmail.com>
2023-10-05 11:04:07 -0300

    criando os card e css na tela inicial.

--- my-app/src/pages/HomeAdmin/index.jsx
+++ my-app/src/pages/HomeAdmin/index.jsx
@@ -8,19 +8,28 @@ export const HomeAdmin = () => {
     return(
         <LayoutPage>
             <header className="main-header"> Sistema de Gestão</header>
+            <div className="title-page">Gestão Geral</div>
             <div className="card">
-                <Link to="/admin/cadastro-aluno" className="btn btn-home">
-                    Cadastrar Aluno
-                </Link>
-                <Link to="#" className="btn btn-home">
-                    Cadastrar Aluno
-                </Link>
-                <Link to="#" className="btn btn-home">
-                    Cadastrar Aluno
-                </Link>
-                <Link to="#" className="btn btn-home">
-                    Cadastrar Aluno
-                </Link>
+                <div className="card-item">
+                    <span className="card-title">Aluno</span>
+                    <span className="card-texto">Define o processo de cadastro de alunos</span>
+                    <Link to="/admin/cadastro-aluno" className="btn btn-home">Acessar</Link>
+                </div>
+                <div className="card-item">
+                    <span className="card-title">Instrutor</span>
+                    <span className="card-texto">Define o processo de cadastro de instutores</span>
+                    <Link to="/admin/cadastro-aluno" className="btn btn-home">Acessar</Link>
+                </div>
+                <div className="card-item">
+                    <span className="card-title">Curso</span>
+                    <span className="card-texto">Define os cursos de programação que serão oferecidos para os alunos</span>
+                    <Link to="/admin/cadastro-aluno" className="btn btn-home">Acessar</Link>
+                </div>
+                <div className="card-item">
+                    <span className="card-title">Matricula</span>
+                    <span className="card-texto">Define o processo de inscrição do aluno em um curso</span>
+                    <Link to="/admin/cadastro-aluno" className="btn btn-home">Acessar</Link>
+                </div>
             </div>
         </LayoutPage>
     );
