5fd30aa6597e17ddb4bb2cfb42254c4812688a63
leal <matheusskeal98@gmail.com>
2023-10-23 17:50:41 -0300

    alterações

--- my-app/src/pages/Student/index.jsx
+++ my-app/src/pages/Student/index.jsx
@@ -1,63 +1,107 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Modal } from "../../components/Modal";
 import "../../assets/globalPage.css";
 import { LayoutPage } from "../../components/LayoutPage";
 
-export const Student = () => {
+class Student extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            studentsList: [],
+            modalOpen: false,
+        }
+    }
 
-    const [modalOpen, setModalOpen] = useState(false);
+    fetchStudentList = () => {
+        fetch(`http://localhost:8080/alunos/list`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            this.setState({
+                studentsList: data
+            });
+        })
+        .catch((errror) => {
+            alert("Desculpe, mas não conseguimos estabelecer conexão com nosso servidor.")
+        });
+    }
 
-    return(
-        <LayoutPage>
-            <div className="page-main-header">
-                <button className="btn-page" onClick={()=> { setModalOpen(true); }}>Novo Aluno</button>
-            </div>
-            <div className="page-body">
-                <div className="page-table">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>RA</th>
-                                <th>Nome</th>
-                                <th>Email</th>
-                                <th>Nascimento</th>
-                                <th class="acao">Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+    openModal = () => {
+        this.setState({ modalOpen: true });
+    }
 
-                        </tbody>
-                    </table>
+    // Function to close the modal
+    closeModal = () => {
+        this.setState({ modalOpen: false });
+    }
+    
+    render() {
+        return(
+            <LayoutPage>
+                <div className="page-main-header">
+                    <button className="btn-page" onClick={this.openModal}>Novo Aluno</button>
                 </div>
-            </div>
-            <Modal isOpen={modalOpen}>
-                <form id="studentForm" className="page-form" action="" method="post">
-                    <div className="page-form-group">
-                        <label htmlFor="ra">RA</label>
-                        <input required type="number" name="ra" id="ra"
-                        />
+                <div className="page-body">
+                    <div className="page-table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>RA</th>
+                                    <th>Nome</th>
+                                    <th>Email</th>
+                                    <th>Nascimento</th>
+                                    <th class="acao">Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                             {
+                                this.state.studentsList.map((alunos) => {
+                                    return (
+                                        <tr key={alunos.id}>
+                                            <td>{alunos.ra}</td>
+                                            <td>{alunos.nome}</td>
+                                            <td>{alunos.email}</td>
+                                            <td>{alunos.dtNascimento}</td>
+                                        </tr>
+                                    )
+                                })
+                             }   
+                            </tbody>
+                        </table>
                     </div>
-                    <div className="page-form-group">
-                        <label htmlFor="nome">Nome</label>
-                        <input required type="text" name="nome" id="nome"
-                        />
-                    </div>
-                    <div className="page-form-group">
-                        <label htmlFor="email">Email</label>
-                        <input required type="email" name="email" id="email"
-                        />
-                    </div>
-                    <div className="page-form-group">
-                        <label htmlFor="date">Data de Nascimento</label>
-                        <input required type="date" name="date" id="date"
-                        />
-                    </div>
-                    <div className="actions">
-                        <button className="btn btn-save">Salvar</button>
-                        <button onClick={() => { setModalOpen(false); }} className="btn btn-cancel">Cancelar</button>
-                    </div>
-                </form>
-            </Modal>
-        </LayoutPage>
-    );
+                </div>
+                <Modal isOpen={this.state.modalOpen}>
+                    <form id="studentForm" className="page-form" action="" method="post">
+                        <div className="page-form-group">
+                            <label htmlFor="ra">RA</label>
+                            <input required type="number" name="ra" id="ra"
+                            />
+                        </div>
+                        <div className="page-form-group">
+                            <label htmlFor="nome">Nome</label>
+                            <input required type="text" name="nome" id="nome"
+                            />
+                        </div>
+                        <div className="page-form-group">
+                            <label htmlFor="email">Email</label>
+                            <input required type="email" name="email" id="email"
+                            />
+                        </div>
+                        <div className="page-form-group">
+                            <label htmlFor="date">Data de Nascimento</label>
+                            <input required type="date" name="date" id="date"
+                            />
+                        </div>
+                        <div className="actions">
+                            <button className="btn btn-save">Salvar</button>
+                            <button onClick={this.closeModal} className="btn btn-cancel">Cancelar</button>
+                        </div>
+                    </form>
+                </Modal>
+            </LayoutPage>
+        );
+    }
 }
+
+export default Student;
--- my-app/src/routes/index.jsx
+++ my-app/src/routes/index.jsx
@@ -4,7 +4,7 @@ import { Register } from "../pages/Register";
 import { PrivateRoute } from "./privateRoutes";
 import { Home } from "../pages/Home";
 import {HomeAdmin} from "../pages/HomeAdmin";
-import { Student } from "../pages/Student";
+import  Student  from "../pages/Student";
 import { Instructor } from "../pages/Instructor";
 import { Teste } from "../pages/teste";
 
